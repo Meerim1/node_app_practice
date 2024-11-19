40f462728b4d4556c74a3bd59115458d805896c4
Айтикеева Мээрим Бекболотовна <meerim.aitikeeva@gmail.com>
2024-11-19 17:29:22 +0600

    routing request, parsing req data (buffers)

--- app.js
+++ app.js
@@ -1,16 +1,43 @@
-const http = require('http');
+const http = require("http");
+const fs = require("fs");
 
 const rqListener = (req, res) => {
-    console.log(req.url, req.method, req.headers);
-    // process.exit();
+  console.log(req.url, req.method, req.headers);
+  const url = req.url;
+  const method = req.method;
 
-    res.setHeader('Content-Type', 'text/html');
-    res.write('<html>');
-    res.write('<head><title>My app title</title></head><body><h1>Test Title</h1></body>');
-    res.write('</html>');
-    res.end();
+  if (url === "/") {
+    res.setHeader("Content-Type", "text/html");
+    res.write("<html>");
+    res.write("<head><title>Enter message</title></head>");
+    res.write(
+      "<body><form action='/message' method='POST'><input type='text' name='someMessage'><button type='submit'>Send</button>Hello from my Node.js Server!</form></body>"
+    );
+    res.write("</html>");
+    return res.end();
+  }
+
+  if (url === "/message" && method === "POST") {
+    const body = [];
+    req.on("data", (chunk) => {
+        body.push(chunk);
+    });
+    req.on('end', () => {
+        const parsedBody = Buffer.concat(body).toString();
+        const msg = parsedBody.split('=')[1];
+        fs.writeFileSync("message.text", msg);
+    })
+    res.statusCode = 302;
+    res.setHeader("Location", "/");
+    return res.end();
+  }
+  res.setHeader("Content-Type", "text/html");
+  res.write("<html>");
+  res.write("<head><title>My app title</title></head>");
+  res.write("<body><h1>Hello from my Node.js Server!</h1></body>");
+  res.write("</html>");
 };
 
 const server = http.createServer(rqListener);
 
-server.listen(8000)
+server.listen(8000);
